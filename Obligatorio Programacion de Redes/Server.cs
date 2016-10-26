95112b22bbb52f34953341963b27952faa72b740
palmera <alegru@gmail.com>
2016-10-25 21:43:34 -0300

    prueba 2 bajar

--- Obligatorio Programacion de Redes/Server.cs
+++ Obligatorio Programacion de Redes/Server.cs
@@ -27,7 +27,7 @@ namespace Servidor
             serverFiles = new List<Files>();
             string[] filesPath = Directory.GetFiles(startupPath);
             string[] fileNames = GetFileNames(startupPath);
-            var prueba ="chau";
+            var prueba ="chau";asfsfsadf
             for (int i = 0; i<fileNames.Length; i++)
             {
                 Files f = new Files();
