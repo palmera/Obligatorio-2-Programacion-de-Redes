20ced8e140a02856a153a567a30faa033e623561
agustin <agustin@moonideas.com>
2016-11-20 16:39:17 -0300

    agus

---
+++ Obligatorio Programacion de Redes/RemotingServices/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Models;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.Exceptions;
+
+namespace Servidor
+{
+    public class UserService : IUserService
+    {
+        private UserData userData;
+        public UserService() {
+            userData = UserData.getInstance();
+        }
+        public void AddUser(string name, string password)
+        {
+            Administrator user = new Administrator(name, password);
+            if (!userData.Exists(name))
+                userData.Add(user);
+            else
+                throw new UserException("Ya existe un usuario con ese nombre");
+        }
+
+        public bool Login(string name, string password)
+        {
+            Administrator user = new Administrator(name, password);
+            return userData.UserLogin(user);
+        }
+
+        public bool Update(string originalName,string newName,string password)
+        {
+            if (!userData.Exists(newName))
+            {
+                Administrator user = new Administrator(newName, password);
+                return userData.Update(originalName, user);
+            }
+            else
+            {
+                throw new UserException("Ya existe un usuario con ese nombre");
+            }
+        }
+
+        public bool Delete(string name)
+        {
+            return userData.Delete(name);
+        }
+
+    }
+}
---
+++ Obligatorio Programacion de Redes/UserData.cs
@@ -0,0 +1,66 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    public class UserData
+    {
+        private static UserData userData;
+        private static Object userDataLock = new Object();
+        public List<Administrator> users { get; set; }
+
+        public static UserData getInstance() {
+            if (userData == null)
+                userData = new UserData();
+            return userData;
+        }
+        private UserData() {
+            users = new List<Administrator>();
+        }
+
+        public bool UserLogin(Administrator  user)
+        {
+            return users.Any(a => a.Name.Equals(user.Name) && a.Password.Equals(user.Password));
+        }
+        public void Add(Administrator user) {
+            lock (userDataLock)
+            {
+                users.Add(user);
+            }
+        }
+        public bool Update(string name,Administrator user) {
+            lock(userDataLock){
+                int index = users.FindIndex((u => u.Name.Equals(name)));
+                Administrator element = users.ElementAt(index);
+                if (element != null)
+                {
+                    element.Name = user.Name;
+                    element.Password = user.Password;
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool Delete(string name)
+        {
+            lock (userDataLock) { 
+                int index = users.FindIndex((u => u.Name.Equals(name)));
+                Administrator element = users.ElementAt(index);
+                if (element != null)
+                {
+                    users.Remove(element);
+                    return true;
+                }
+                return false;
+            }
+
+        }
+        public bool Exists(string name) {
+            return users.Exists(u => u.Name.Equals(name));
+        }
+    }
+}
