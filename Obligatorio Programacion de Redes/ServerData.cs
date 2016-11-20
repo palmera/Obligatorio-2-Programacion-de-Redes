7bdf57d8ded8cfa1450a7957cb44562d48f1236a
palmer <alegru@gmail.com>
2016-11-20 16:44:02 -0300

    s

--- Obligatorio Programacion de Redes/ServerData.cs
+++ Obligatorio Programacion de Redes/ServerData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Obligatorio_Programacion_de_Redes.Models;
+using Servidor.Models;
 
 namespace Servidor
 {
