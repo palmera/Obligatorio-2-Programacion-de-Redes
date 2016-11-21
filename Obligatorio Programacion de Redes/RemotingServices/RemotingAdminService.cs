cb5603befd0bd1eea0ee9ef8049c99fa91f60574
palmer <alegru@gmail.com>
2016-11-21 20:39:21 -0300

    dsf

--- Obligatorio Programacion de Redes/RemotingServices/RemotingAdminService.cs
+++ Obligatorio Programacion de Redes/RemotingServices/RemotingAdminService.cs
@@ -16,21 +16,21 @@ namespace Servidor
         public bool LoginAdmin(string name, string password) {
             var admin = new Administrator(name, password);
             var a = SD.AdminLogin(admin);
-            LoggerSender.Log("Admin login:\r\n Username: "+name+"\r\n Password: "+password+"\r\n");
+            LoggerSender.Log("Admin login: Username: "+name+" Password: " +password);
             return a;
         }
         public bool AddAdmin(string name, string password) {
             var admin = new Administrator(name, password);
-            LoggerSender.Log("Create Admin:\r\n Username: " + name + "\r\n Password: " + password + "\r\n");
+            LoggerSender.Log("Create Admin:Username: " + name + "Password: " + password);
             return SD.AddAdmin(admin);
         }
         public bool ModifyAdmin(string name, string newName) {
-            LoggerSender.Log("Modify Admin:\r\n Old name: " + name + "\r\n New name: " + newName + "\r\n");
+            LoggerSender.Log("Modify Admin:Old name: " + name + "New name: " + newName);
             return SD.ModifyAdmin(name, newName);
         }
 
         public bool DeleteAdmin(string name) {
-            LoggerSender.Log("Delete Admin:\r\n Username: " + name + "\r\n");
+            LoggerSender.Log("Delete Admin: Username: " + name);
             return SD.DeleteAdmin(name);
         }
 
